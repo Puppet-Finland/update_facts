85d9b72ab95619825c420566f5c52e2b7b0d660c
Samuli Seppänen <samuli.seppanen@gmail.com>
2014-11-20 18:36:16 +0200

    Fixes to rdoc
    Signed-off-by: Samuli Seppänen <samuli.seppanen@gmail.com>

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,12 +7,12 @@
 # == Authors
 #
 # Samuli Seppänen <samuli.seppanen@gmail.com>
+#
 # Samuli Seppänen <samuli@openvpn.net>
 #
 # == License
 #
-# BSD-lisence
-# See file LICENSE for details
+# BSD-license. See file LICENSE for details.
 #
 class update_facts {
     # Empty
